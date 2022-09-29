5aa3b535f0cb4876297ce12448f16eb41dfab725
KevinKra <kratok08@gmail.com>
2022-09-28 21:33:52 -0400

    def func to determine game length

--- src/classes/game.py
+++ src/classes/game.py
@@ -8,6 +8,7 @@ class Game:
 
     def __init__(self) -> None:
         self.player = Type[Player]
+        self.story_length = 0
 
     def create_player(self) -> None:
         """initializes the player"""
@@ -17,6 +18,21 @@ class Game:
         self.player.set_attribute_points(5)
         self.player.spend_player_attributes()
 
+    def set_story_length(self) -> None:
+        """set the length of the game (story)"""
+        response = input("Choose a story length [short, medium, long]")
+        while response.lower() not in ["short", "medium", "long"]:
+            response = input("Enter either: short, medium, or long")
+        if response == "short":
+            self.story_length = 10
+        elif response == "medium":
+            self.story_length = 25
+        elif response == "long":
+            self.story_length = 50
+        else:
+            raise ValueError("Unexpected user response")
+
     def start_game(self) -> None:
         """root function that starts the game"""
         self.create_player()
+        self.set_story_length()
---
+++ src/classes/role.py
@@ -0,0 +1,20 @@
+"""module doc string"""
+
+
+class Role:
+    """role"""
+
+    def __init__(self) -> None:
+        pass
+
+    def attack(self) -> None:
+        """attack"""
+        print("attack")
+
+    def defend(self) -> None:
+        """defend"""
+        print("defend")
+
+    def run_away(self) -> None:
+        """run away"""
+        print("run away")
